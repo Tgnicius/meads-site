e2f48439e94aad9d13fd6a80d319b2dfcd6a564d
Lucius Vinicius <luciusviniciusf@ua.pt>
2020-05-28 17:04:03 +0100

    Separação Empresa e Cliente em 'minha conta'

--- files/theme/files/knock.js
+++ files/theme/files/knock.js
@@ -1,13 +1,13 @@
 function vm() {
     try {
-        console.log("try");
         this.isLoged = ko.observable(JSON.parse(localStorage.getItem("logado")));
         console.log(this.isLoged);
     }
     catch (err){
-        console.log("catch");
         this.isLoged = ko.observable(false);
     }
+    this.cliente = ko.observable(JSON.parse(localStorage.getItem("cliente")));
+    console.log(this.conta);
  
     logar = function() {
         localStorage.setItem("contas", JSON.stringify([{name: "noobmaster69", email: "cliente@email.com", pass:"cliente"}, {name: "Ubisoft", email: "empresa@email.com", pass:"empresa"}]));
@@ -15,19 +15,25 @@ function vm() {
         var isValid = false;
         for (let i=0; i<contas.length; i++){
             if ($("#email").val() == contas[i].email && $("#pass").val() == contas[i].pass ){
-                localStorage.setItem("conta", JSON.stringify({name: contas[i].name, email: contas[i].email, pass: contas[i].pass}));
+                if (contas[i].pass == "cliente"){
+                    localStorage.setItem("cliente", JSON.stringify(true));
+                }
+                else{
+                    localStorage.setItem("cliente", JSON.stringify(false));
+                }
+                
+                this.cliente(contas[i].pass);
                 $("#invalid_login").hide();
                 this.isLoged(true);
                 localStorage.setItem("logado", JSON.stringify(true));
                 isValid=true;
-            }
+            }   
         }
         if (!isValid){
             $("#invalid_login").show();
         }
     }
     deslogar = function() {
-        console.log("deslogou");
         localStorage.setItem("logado", JSON.stringify(false));
         this.isLoged(false);
     }
