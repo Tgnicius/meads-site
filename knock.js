20d97dc2774af8fc7f7b00f7180ce3dd26f9aabc
Lucius Vinicius <luciusviniciusf@ua.pt>
2020-06-06 12:58:10 +0100

    KO para produtos que levam à sua página

--- knock.js
+++ knock.js
@@ -7,6 +7,17 @@ function vm() {
         this.isLoged = ko.observable(false);
     }
     this.cliente = ko.observable(JSON.parse(localStorage.getItem("cliente")));
+
+    this.jogos = ko.observable(JSON.parse(localStorage.getItem("jogos")));
+    this.numJogos = ko.observable(JSON.parse(localStorage.getItem("numJogos")));
+    this.produtos = ko.observable(JSON.parse(localStorage.getItem("produtos")));
+    this.numProdutos = ko.observable(JSON.parse(localStorage.getItem("numProdutos")));
+
+    this.produto = ko.observable(JSON.parse(localStorage.getItem("produto")));
+    this.jogo = ko.observable(JSON.parse(localStorage.getItem("jogo")));
+
+    this.img = ko.observable();
+
  
     logar = function() {
         localStorage.setItem("contas", JSON.stringify([{name: "noobmaster69", email: "cliente@email.com", pass:"cliente"}, {name: "Ubisoft", email: "empresa@email.com", pass:"empresa"}]));
@@ -39,8 +50,9 @@ function vm() {
 
     // Empresa add jogos ----------------------
 
-    this.game = ko.observable();
-    this.merch = ko.observable();
+    this.game = ko.observable(false);
+    this.merch = ko.observable(false);
+    this.verForm = ko.observable(false);
 
     showGame = function() {
         this.game(true);
@@ -52,6 +64,100 @@ function vm() {
         this.merch(true);
         this.game(false);
         return true // O radio vai ficar checked
+    }
+
+    verifyForm = function() {
+        var isValid = true;
+        if ($.trim($("#prodName").val()).length < 2 || $.trim($("#prodPrice").val()).length < 1 || 
+        $.trim($("#prodDesc").val()).length < 2 || $.trim($("#prodType").val()).length < 2){
+            isValid = false;
+        }
+        else {
+            if (!$("#game").is(":checked") && !$("#merch").is(":checked")){
+                isValid = false; // Tem q ter pelo menos um dos radios marcados
+            }
+
+            if ($("#game").is(":checked") && $.trim($("#gameAge").val()).length < 1) { 
+                isValid = false;
+            }
+
+            if ($("#merch").is(":checked") && ($.trim($("#mHeight").val()).length < 1 || $.trim($("#mWidth").val()).length < 1 || 
+            $.trim($("#mLength").val()).length < 1)) {
+                isValid = false;
+            } 
+        }
+
+        this.verForm(isValid);
+
+    }
+
+    submitForm = function() {
+
+
+        var name = $("#prodName").val();
+        var price = $("#prodPrice").val();
+        var desc = $("#prodDesc").val();
+        var tipo = $("#prodType").val();
+
+        if ($("#game").is(":checked")){
+            var age = $("#gameAge").val();
+            var nJogo = parseInt(localStorage.getItem("numJogos"));
+            if (isNaN(nJogo)){
+                nJogo = 0;
+            }
+            nJogo++;
+
+
+            var jogos = new Array();
+            console.log(localStorage.getItem("jogos"));
+            jogos = JSON.parse(localStorage.getItem("jogos")) || []; // dados dos jogos
+            var jogo = {nome: name, preco: price, description: desc, genero: tipo, idade: age};
+            jogos.push(jogo); // adc o jogo para a lista
+            localStorage.setItem("jogos", JSON.stringify(jogos)); // salvar a lista no storage
+            localStorage.setItem("numJogos", JSON.stringify(nJogo));
+            $("#nJog").text(nJogo);
+        }
+
+        if ($("#merch").is(":checked")){
+
+
+            var height = $("#mHeight").val();
+            var width = $("#mWidth").val();
+            var leng = $("#mLength").val();
+            var produtos = new Array();
+            var nProd = parseInt(localStorage.getItem("numProdutos"));
+            if (isNaN(nProd)){
+                nProd = 0;
+            }
+            nProd++;
+
+            if (localStorage.getItem("produtos") != "undefined")
+                produtos = JSON.parse(localStorage.getItem("produtos")) || []; // dados dos jogos
+            var produto = {nome: name, preco: price, description: desc, genero: tipo, altura: height, largura: width, comprimento: leng};
+            produtos.push(produto); // adc o jogo para a lista
+            localStorage.setItem("produtos", JSON.stringify(produtos)); // salvar a lista no storage
+            localStorage.setItem("numProdutos", JSON.stringify(nProd));
+            $("#nProd").text(nProd);
+            console.log((localStorage.getItem("numProdutos")))
+            console.log(produtos);
+
+        }
+    }
+
+    // Empresa remove produtos
+
+
+
+    // Produtos e Jogos página de compras -------------------------
+
+    this.produtosPage = function(produto) {
+        localStorage.setItem("produto", JSON.stringify(produto));
+        document.location.href = "produto-aleatorio.html" // mudar url da página
+    }
+
+    this.jogosPage = function(jogo){
+        localStorage.setItem("jogo", JSON.stringify(jogo));
+        document.location.href = "jogo-aleatorio.html";
     }
 
 
